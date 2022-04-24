cc84f7743585d5410a83fb55b882d8a2e949354b
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-04-24 13:57:49 +0300

    added a test function that tests if the functionin user can save users.

--- user_test.py
+++ user_test.py
@@ -11,9 +11,9 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.username, 'vonmak')
         self.assertEqual(self.new_user.password, '123456')
 
-#     def test_save_user(self):
-#         self.new_user.save_user()
-#         self.assertEqual(len(User.user_list), 1)
+    def test_save_user(self):
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list), 1)
 
 
 if __name__ == '__main__':
