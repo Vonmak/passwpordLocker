e266e2a02faec5f434dd126bc516ea884d8aa68c
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-04-24 13:52:35 +0300

    initialized a function setup and testcases and the unitest which helps functions to run.

--- user_test.py
+++ user_test.py
@@ -1,20 +1,20 @@
-# import unittest
-# from user import User
+import unittest
+from user import User
 
 
-# class TestUser(unittest.TestCase):
+class TestUser(unittest.TestCase):
 
-#     def setUp(self):
-#         self.new_user = User('vonmak', '123456')
+    def setUp(self):
+        self.new_user = User('vonmak', '123456')
 
-#     def test_init(self):
-#         self.assertEqual(self.new_user.username, 'vonmak')
-#         self.assertEqual(self.new_user.password, '123456')
+    def test_init(self):
+        self.assertEqual(self.new_user.username, 'vonmak')
+        self.assertEqual(self.new_user.password, '123456')
 
 #     def test_save_user(self):
 #         self.new_user.save_user()
 #         self.assertEqual(len(User.user_list), 1)
 
 
-# if __name__ == '__main__':
-#     unittest.main()
+if __name__ == '__main__':
+    unittest.main()
