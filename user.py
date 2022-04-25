96fc7bb7b7d5ca45374175b77ab835bdbc8fb74c
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-04-25 12:24:56 +0300

    added functions to check for existing users and inserted docstrings.

--- user.py
+++ user.py
@@ -18,3 +18,20 @@ class User:
         function to save users
         '''
         User.user_list.append(self)
+
+    @classmethod
+    def user_exist(cls, username):
+        '''
+        function to check wether credentials exist
+        '''
+        for user in cls.user_list:
+            if user.username == username:
+                return True
+        return False
+
+    @classmethod
+    def display_users(cls):
+        '''
+        function to display credentials
+        '''
+        return cls.user_list
