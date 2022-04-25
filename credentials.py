7f60e299dfc50a7f1ec612a8e8cc7e44ef9f4638
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-04-25 10:23:33 +0300

    added docstrings to to credentials.py

--- credentials.py
+++ credentials.py
@@ -3,24 +3,39 @@ class Cred:
     accounts_list = []
 
     def __init__(self, account, account_username, account_password):
+        '''
+        function to initialize parameters
+        '''
         self.account = account
         self.account_username = account_username
         self.account_password = account_password
 
     def save_cred(self):
+        '''
+        function to save credentials
+        '''
         Cred.accounts_list.append(self)
 
     def delete_cred(self):
+        '''
+        function to delete credentials
+        '''
         Cred.accounts_list.remove(self)
 
     @classmethod
     def find_cred(cls, account):
+        '''
+        function to find credentials
+        '''
         for cred in cls.accounts_list:
             if cred.account == account:
                 return cred
 
     @classmethod
     def cred_exist(cls, account):
+        '''
+        function to check wether credentials exist
+        '''
         for cred in cls.accounts_list:
             if cred.account == account:
                 return True
@@ -28,4 +43,7 @@ class Cred:
 
     @classmethod
     def display_cred(cls):
+        '''
+        function to display credentials
+        '''
         return cls.accounts_list
