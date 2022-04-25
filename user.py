222ff5ff1b3517ef3c1519aa4f6f18c3a96c019a
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-04-25 10:20:05 +0300

    added docstrings to to user.py

--- user.py
+++ user.py
@@ -7,9 +7,14 @@ class User:
     user_list = []
 
     def __init__(self, username, password):
+        '''
+        function to initialize parameters
+        '''
         self.username = username
         self.password = password
 
     def save_user(self):
-
+        '''
+        function to save users
+        '''
         User.user_list.append(self)
